df872cd88d32857456fcc6703b17461c25969a9d
ExStansReal <yurakoshke@mail.ru>
2022-02-21 20:20:01 +0300

    Обновление модели и класса PowerSupply, а именно добавление конвертеров

--- LeftCard/ViewModels/LeftCardViewModel.cs
+++ LeftCard/ViewModels/LeftCardViewModel.cs
@@ -14,64 +14,41 @@ namespace LeftCard.ViewModels
 {
     public class LeftCardViewModel : ViewModelBase
     {
-        private PowerSupply _items;
+        private PowerSupply _powerSupply;
+
+        public PowerSupply PowerSupply
+        {
+            get => _powerSupply;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _powerSupply, value);
+                this.RaisePropertyChanged(nameof(TurningOffOnCommand));
+                this.RaisePropertyChanged(nameof(TemperatureCard));
+                this.RaisePropertyChanged(nameof(ColorTempCard));
+                this.RaisePropertyChanged(nameof(KelvincTempCard));
+                this.RaisePropertyChanged(nameof(VoltazAndPowerCard));
+                this.RaisePropertyChanged(nameof(ControllerAndChannelCard));
+            }
+        }
         public LeftCardViewModel(PowerSupply items)
         {
-            _items = items;
-            TemperatureCard = Convert.ToString(items.temperature) + "°C";
-                if (items.temperature <= 30)
-                    ColorTempCard = "Black";
-                else if (items.temperature <= 65)
-                    ColorTempCard = "Orange";
-                else if (items.temperature <= 90)
-                    ColorTempCard = "Red";
-                else if (items.temperature > 90)
-                    ColorTempCard = "DarkRed";
-
-                KelvincTempCard = Convert.ToString((items.temperature + 273, 15)) + "k";
-                VoltazAndPowerCard = Convert.ToString(items.voltage) + "V | " + Convert.ToString(items.current_strength) + "A";
-                NameCard = items.name;
-                ControllerAndChannelCard = items.controller.productionName + " | Канал" + Convert.ToString(items.id);
-                PlacementCard = items.Placement;
+            PowerSupply = items;
+            TemperatureCard = items.TemperatureCard;
+            ColorTempCard = items.Color;
+            KelvincTempCard = items.KelvincTempCard;
+            VoltazAndPowerCard = items.VoltazAndPowerCard;
+            NameCard = items.name;
+            ControllerAndChannelCard = items.ControllerAndChannelCard;
+            PlacementCard = items.Placement;
+            TurningOffOnCommand = items.TurningOffOnCommand;
         }
-
-        public ICommand TurningOffOnCommand
-        {
-            get => ReactiveCommand.CreateFromTask(async () =>
-            {
-                switch (_items.workStatus)
-                {
-                    case WorkStatus.On:
-                        await _items.start();
-                        break;
-                    case WorkStatus.Off:
-                        await _items.stop();
-                        break;
-                }
-            });
-        }
-
-        private string _temperatureCard;
-        public string TemperatureCard { get => _temperatureCard; set => this.RaiseAndSetIfChanged(ref _temperatureCard, value); }
-
-        private string _colortempcard;
-        public string ColorTempCard { get => _colortempcard; set => this.RaiseAndSetIfChanged(ref _colortempcard, value); }
-
-        private string _kelvinctempcard;
-        public string KelvincTempCard { get => _kelvinctempcard; set => this.RaiseAndSetIfChanged(ref _kelvinctempcard, value); }
-
-        private string _voltazandpowercard;
-        public string VoltazAndPowerCard { get => _voltazandpowercard; set => this.RaiseAndSetIfChanged(ref _voltazandpowercard, value); }
-
-        private string _namecard;
-        public string NameCard { get => _namecard; set => this.RaiseAndSetIfChanged(ref _namecard, value); }
-
-        private string _controllerandchannelcard;
-        public string ControllerAndChannelCard { get => _controllerandchannelcard; set => this.RaiseAndSetIfChanged(ref _controllerandchannelcard, value); }
-
-        private string _placementcard;
-        public string PlacementCard { get => _placementcard; set => this.RaiseAndSetIfChanged(ref _placementcard, value); }
-
-        
+        public ICommand TurningOffOnCommand{ get;set;}
+        public string TemperatureCard { get; set; }
+        public string ColorTempCard { get; set; }
+        public string KelvincTempCard { get; set; }
+        public string VoltazAndPowerCard { get; set; }
+        public string NameCard { get; set; }
+        public string ControllerAndChannelCard { get; set; }
+        public string PlacementCard { get; set; }
     }
 }
--- LeftCard/ТЗ/PowerSupply.cs
+++ LeftCard/ТЗ/PowerSupply.cs
@@ -1,12 +1,15 @@
 ﻿
 using DALI.Enum;
+using ReactiveUI;
+using System;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace DALI.Models
 {
     public class PowerSupply
     {
-      
+        
         public PowerSupply(string idi, int temp, int vol, int cur_str, string na, Channel channel, Controller cont, WorkStatus work, string _placement)
         {
             id = idi;
@@ -26,19 +29,67 @@ namespace DALI.Models
             Items = items;
         }
 
+        public ICommand TurningOffOnCommand
+        {
+            get => ReactiveCommand.CreateFromTask(async () =>
+            {
+                switch (this.workStatus)
+                {
+                    case WorkStatus.On:
+                        await this.start();
+                        break;
+                    case WorkStatus.Off:
+                        await this.stop();
+                        break;
+                }
+            });
+        }
+
+        public string Color {
+            get
+            {
+                string color = "";
+                if (this.temperature <= 30)
+                    color = "Black";
+                else if (this.temperature <= 65)
+                    color = "Orange";
+                else if (this.temperature <= 90)
+                    color = "Red";
+                else if (this.temperature > 90)
+                    color = "DarkRed";
+                return color;
+            }
+        }
+
         public string id { get; set; }
 
-        public int temperature { get; set; }
+        private int temperature { get; set; }
+        public string TemperatureCard
+        {
+            get => Convert.ToString(this.temperature) + "°C";
+        }
+        public string KelvincTempCard
+        {
+            get => Convert.ToString((this.temperature + 273, 15)) + "k";
+        }
 
-        public int voltage { get; set; }
+        private int voltage { get; set; }
 
-        public int current_strength { get; set; }
+        private int current_strength { get; set; }
+        public string VoltazAndPowerCard
+        {
+            get => Convert.ToString(this.voltage) + "V | " + Convert.ToString(this.current_strength) + "A";
+        }
 
         public string name { get; set; }
 
         public Channel _channel { get; set; }
 
-        public Controller controller { get; set; }
+        private Controller controller { get; set; }
+        public string ControllerAndChannelCard
+        {
+            get => this.controller.productionName + " | Канал" + Convert.ToString(this.id);
+        }
         public string Placement { get; set; }
 
         public WorkStatus workStatus { get; set; }
